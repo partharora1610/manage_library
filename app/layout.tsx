b46079569972bf2990754fabb513cc6d5827c825
partharora1610 <partharora16102001@gmail.com>
2023-12-02 22:00:02 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+// providers
+import "./globals.css";
+
+// styles
+
+// fonts
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+// metadata
+export const metadata: Metadata = {
+  title: "SST_Library",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-accent-blue">
+      <h2 className="text-primary-200 h2-bold">SST LIBRARY</h2>
+    </main>
+  );
+}
